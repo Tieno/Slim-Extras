6368eeb22a161ffe5ceaa317b0e1cdf5d07aab1e
Martijn Durnez <martijndurnez@gmail.com>
2012-04-28 18:23:20 +0200

    added Markdown twig extension
    taken from https://github.com/geta6/Twig-Markdown and adjusted for easy loading in TwigView
    TwigView::$twigExtensions = array('Twig_Extensions_Markdown');

---
+++ Views/Extension/Twig/Extensions/Markdown.php
@@ -0,0 +1,39 @@
+<?php
+
+if (!defined('ENT_SUBSTITUTE')) {
+    define('ENT_SUBSTITUTE', 8);
+}
+
+class Twig_Extensions_Markdown extends Twig_Extension
+{
+    public function getTokenParsers()
+    {
+        include_once dirname(__FILE__).'/Markdown/TokenParser/Markdown.php';
+        return array(
+            new Twig_TokenParser_Markdown(),
+        );
+    }
+
+    public function getFilters()
+    {
+        $filters = array(
+            // formatting filters
+            'markdown'=> new Twig_Filter_Function('twig_markdown'),
+        );
+
+        return $filters;
+    }
+
+    public function getName()
+    {
+        return 'markdown';
+    }
+
+}
+
+function twig_markdown($data)
+{
+    include_once dirname(__FILE__).'/Markdown/Markdown/markdown.php';
+    return Markdown($data);
+}
+
